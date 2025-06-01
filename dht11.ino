19b5c6d51f6a96fd6ebe4404094f350f3bfb409a
arjmarty <annrose0227@gmail.com>
2025-06-01 23:30:00 +0800

    Update dht11.ino with the void loop contents: running of temp and humidity sensor

--- dht11.ino
+++ dht11.ino
@@ -3,20 +3,28 @@
 #include <DHT_U.h>
 
 #define DHTTYPE DHT11
-#define DHTPIN4
-
-
+#define dataPin 4
 
 void setup() {
   Serial.begin(9600); //initialize the device
   dht.begin();
-  Serial.println(F("DHT11 sensor example"))
+  Serial.println(F("DHT11 sensor example"));
 }
 
 void loop() {
-  //Delay between measurements.
-  delay(delayMS);
-  //Get temperature event and print its value
-  sensors_event_t event;
+  int readData = DHT.read11(dataPin);
+  float t = DHT.temperature;
+  float h = DHT.humidity;
 
+  //print the values on the serial monitor
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.print(" *C");
+  Serial.print("Humidity = ");
+  Serial.print(h);
+  Serial.print(" %");
+
+  //Delay between measurements.
+  delay(2000);
+  
 }
